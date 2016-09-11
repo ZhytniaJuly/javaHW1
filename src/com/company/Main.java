ae576cb3f83fbaf8a6a2bf53a84444a02b53af33
ZhytniaJuly <ZhytniaJuly@gmail.com>
2016-09-11 22:39:39 +0300

    Массивы 3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,17 +2,32 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
+
         int[] array;
-        array = new int[8];
+        array = new int[4];
+//      int[] array = {1, 10, 25, 99};
         for (int i = 0; i < array.length; i++) {
-            array[i] = ((int)(Math.random()*10+1) );
+            array[i] = (int)(Math.random()*90+10);
             System.out.print(array[i]+" ");
         }
-        System.out.println();
-        for(int i=0;i<array.length;i++){
-            if(array[i]%2!=0)array[i]=0;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int)(Math.random()*90+10);
             System.out.print(array[i]+" ");
+        }
+            System.out.println();
+            for (int i = 0; i < array.length; i++) {
+                if (i > 0) {
+                    if (array[i - 1] >= array[i]) {
+                        System.out.println("Последовательность не возрастающая");
+                        break;
+                    }
+                }
+                    if (i == array.length - 1)
+                        System.out.println("Последовательность возрастающая");
+
+            }
+
         }
     }
-}
+
 
