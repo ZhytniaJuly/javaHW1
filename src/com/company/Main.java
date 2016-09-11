9cb6d0b3d2aa1bdad9018e619254befefb9c6faf
ZhytniaJuly <ZhytniaJuly@gmail.com>
2016-09-11 12:33:53 +0300

    123456

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,20 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	int i = 2;
-        for (int numElement = 1; numElement <21 ; numElement++) {
-            System.out.println("element"+numElement+"=" + i);
-            i=i*2;
+            int fibonachi [] = new int [11];
+            fibonachi [0] = 1;
+            fibonachi [1] = 1;
+
+            for (int i = 2; i < 11; i++) {
+                fibonachi [i] = fibonachi[i-1]+fibonachi [i-2];
+            }
+
+            for (int i = 0; i < 11; i++) {
+                int j=i+1;
+                System.out.println("fibonachi " + j +" = " +fibonachi [i]);
+            }
         }
+
+
     }
-}
+
