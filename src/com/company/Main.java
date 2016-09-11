18e1f6f4161702d8f939b3834e9dfd39893f6073
ZhytniaJuly <ZhytniaJuly@gmail.com>
2016-09-11 11:34:49 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int i = 2;
+        for (int numElement = 1; numElement <21 ; numElement++) {
+            System.out.println("element"+numElement+"=" + i);
+            i=i*2;
+        }
+    }
+}
